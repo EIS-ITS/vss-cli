5510bffc1eae8cf42d9a6350027f45a972aab2f2
JM Lopez <jm.lopez@utoronto.ca>
2019-02-01 14:24:57 -0500

    Issue #4: Configure CLI with env vars and/or input with configure.py plugin - ls command lists configuration from env and/or file - mk to create new configuration - fixes #4

--- vss_cli/plugins/configure.py
+++ vss_cli/plugins/configure.py
@@ -1,9 +1,14 @@
 """Configuration plugin for VSS CLI (vss-cli)."""
 import click
+import logging
 import os
+from vss_cli import const
 from vss_cli.cli import pass_context
 from vss_cli.config import Configuration
 from vss_cli.helper import format_output
+
+
+_LOGGING = logging.getLogger(__name__)
 
 
 @click.group('config')
@@ -12,7 +17,7 @@ def cli(ctx: Configuration):
     """Configure VSS CLI options. If this command is run with no arguments,
     you will be prompted for configuration values such as your VSS username
     and password.  If your config file does not  exist (the default location
-    is ~/.vss/config.json), the VSS CLI will create it for you."""
+    is ~/.vss-cli/config.json, the VSS CLI will create it for you."""
     ctx.auto_output('table')
 
 
@@ -53,20 +58,25 @@ COLUMNS_DETAILS = [
 @pass_context
 def ls(ctx: Configuration):
     """List existing configuration"""
-    profiles = ctx.load_raw_config_file()
-    _profiles = list()
     from base64 import b64decode
-    # checking profiles
-    for key, profile in profiles.items():
-        auth = profile['auth']
-        auth_enc = auth.encode()
-        user, pwd = b64decode(auth_enc).split(b':')
-        masked_pwd = ''.join(['*' for i in range(len(pwd))])
-        _profiles.append({'endpoint': key,
-                          'user': user.decode(), 'pass': masked_pwd[:8],
-                          'token': '{}...{}'.format(profile['token'][:10],
-                                                    profile['token'][-10:]),
-                          'source': 'config file'})
+    _profiles = list()
+    try:
+        profiles = ctx.load_raw_config_file()
+
+        # checking profiles
+        for key, profile in profiles.items():
+            auth = profile['auth']
+            auth_enc = auth.encode()
+            user, pwd = b64decode(auth_enc).split(b':')
+            masked_pwd = ''.join(['*' for i in range(len(pwd))])
+            _profiles.append({'endpoint': key,
+                              'user': user.decode(), 'pass': masked_pwd[:8],
+                              'token': '{}...{}'.format(profile['token'][:10],
+                                                        profile['token'][-10:]),
+                              'source': 'config file'})
+    except FileNotFoundError as ex:
+        _LOGGING.warning(f'{str(ex)}. Have you run vss-cli configure mk?')
+
     # checking env vars
     envs = [e for e in os.environ if 'VSS_' in e]
     if envs:
@@ -74,7 +84,7 @@ def ls(ctx: Configuration):
         pwd = os.environ.get('VSS_USER_PASS', '')
         masked_pwd = ''.join(['*' for i in range(len(pwd))])
         tk = os.environ.get('VSS_TOKEN', '')
-        endpoint = os.environ.get('VSS_ENDPOINT', '')
+        endpoint = os.environ.get('VSS_ENDPOINT', const.DEFAULT_SERVER)
         source = 'env'
         _profiles.append({'endpoint': endpoint,
                           'user': user, 'pass': masked_pwd,
