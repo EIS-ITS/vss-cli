6434c0f11f7124c79136292c4430f2b4b30e6f4b
JM Lopez <jm.lopez@utoronto.ca>
2024-07-11 15:48:59 -0400

    Issue #705: ♻plugins/implement importlib-resources and importlib-metadata to replace pkg_resources - fixes #705

--- vss_cli/plugins/plugins.py
+++ vss_cli/plugins/plugins.py
@@ -3,7 +3,7 @@ import logging
 
 import click
 from click_plugins import with_plugins
-from pkg_resources import iter_entry_points
+from importlib_metadata import entry_points
 
 from vss_cli.cli import pass_context
 from vss_cli.config import Configuration
@@ -11,7 +11,7 @@ from vss_cli.config import Configuration
 _LOGGING = logging.getLogger(__name__)
 
 
-@with_plugins(iter_entry_points('vss_cli.contrib.plugins'))
+@with_plugins(entry_points(group='vss_cli.contrib.plugins'))
 @click.group('plugins', short_help='External plugins.')
 @pass_context
 def cli(ctx: Configuration):
