151b30cdc9bf017217adf5314ba146ac95a89c37
JM Lopez <jm.lopez@utoronto.ca>
2026-01-14 13:26:22 -0500

    Issue #810: 📦Homebrew formula for vss-cli v2026.1.0 - fixes #810

--- HomebrewFormula/vss-cli.rb
+++ HomebrewFormula/vss-cli.rb
@@ -3,8 +3,8 @@ class VssCli < Formula
 
   desc "ITS Private Cloud Command Line Interface vss-cli"
   homepage "https://eis.utoronto.ca/~vss/vss-cli"
-  url "https://files.pythonhosted.org/packages/9f/b1/e9cbb5c0a9273c432f8117f14ea7c0aa2b091a08d8b8af553819b08dc663/vss_cli-2025.10.0-py2.py3-none-any.whl"
-  sha256 "79b79c06baecb000630011c490c8044c560632cde1d4506156aa4d54464fecc8"
+  url "https://files.pythonhosted.org/packages/4d/63/91cb18507d907f0b9603dcdc624243f813a1dc218196448703c2425df53a/vss_cli-2026.1.0-py2.py3-none-any.whl"
+  sha256 "057caf1af209df2d8708bf9705903a5d63bf7827a4e2c2a77aa19d823d960278"
   license "MIT"
 
   depends_on "python@3.11"
